9d85e4d2a6f2197d0b61d9e97a4dbded3510649a
kimyw2353 <kimyw2353@naver.com>
2022-10-28 16:17:13 +0900

    반복문 사용

--- Part1/freight.php
+++ Part1/freight.php
@@ -1,26 +1,50 @@
 <!DOCTYPE html>
 <html>
-  <head>
-   <title>Bob's Auto Parts - Freight Costs</title>
-  </head>
-  <body>
-    <table style="border: 0px; padding: 3px">
+<head>
+    <title>Bob's Auto Parts - Freight Costs</title>
+</head>
+<body>
+<table style="border: 0px; padding: 3px">
     <tr>
-     <td style="background: #cccccc; text-align: center;">Distance</td>
-     <td style="background: #cccccc; text-align: center;">Cost</td>
+        <td style="background: #cccccc; text-align: center;">Distance</td>
+        <td style="background: #cccccc; text-align: center;">Cost</td>
     </tr>
 
+    <!--while 문 사용-->
     <?php
-    $distance = 50;
-    while ($distance <= 250) {
-      echo "<tr>
-            <td style=\"text-align: right;\">".$distance."</td>
-            <td style=\"text-align: right;\">".($distance / 10)."</td>
-            </tr>\n";
-      $distance += 50;
-    }
+        $distance = 50;
+        while ($distance <= 250) {
+            echo "<tr>
+                <td style=\"text-align: right; \">" . $distance . "</td>
+                <td style=\"text-align: right; \">" . ($distance / 10) . "</td>
+                </tr>\n";
+            $distance += 50;
+        }
     ?>
-    
-    </table>
-  </body>
+
+</table>
+<hr>
+<table style="border: 0px; padding: 3px">
+    <tr>
+        <td style="background: #cccccc; text-align: center;">Distance</td>
+        <td style="background: #cccccc; text-align: center;">Cost</td>
+    </tr>
+
+    <!--for 문 사용-->
+    <?php
+    for ($distance = 50; $distance<= 250; $distance += 50) {
+        echo "<tr>
+        <td style=\"text-align: right; \">".$distance."</td>
+        <td style=\"text-align: right; \">".($distance / 10)."</td>
+        </tr>\n";
+    }
+?>
+</table>
+<?php
+    $num = 30;
+    do {
+        echo $num."<br />";
+    } while ($num < 1);
+?>
+</body>
 </html>
